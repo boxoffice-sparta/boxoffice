17eda25e8d6aa6d6d99109f3e8b4e066cf5333d4
주원 <juwon_0715@naver.com>
2026-05-22 11:25:20 +0900

    feat: 업체 상세 조회 응답 및 서비스 추가

--- company-service/src/main/java/com/boxoffice/companyservice/company/service/CompanyService.java
+++ company-service/src/main/java/com/boxoffice/companyservice/company/service/CompanyService.java
@@ -1,12 +1,17 @@
 package com.boxoffice.companyservice.company.service;
 
+import com.boxoffice.common.exception.BaseException;
 import com.boxoffice.companyservice.company.dto.request.CompanyCreateRequestDto;
 import com.boxoffice.companyservice.company.dto.response.CompanyCreateResponseDto;
+import com.boxoffice.companyservice.company.dto.response.CompanyResponseDto;
 import com.boxoffice.companyservice.company.entity.Company;
+import com.boxoffice.companyservice.company.exception.CompanyErrorCode;
 import com.boxoffice.companyservice.company.repository.CompanyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -26,5 +31,13 @@ public class CompanyService {
         Company savedCompany = companyRepository.save(company);
 
         return CompanyCreateResponseDto.from(savedCompany);
+    }
+
+    @Transactional(readOnly = true)
+    public CompanyResponseDto getCompany(UUID companyId) {
+        Company company = companyRepository.findById(companyId)
+                .orElseThrow(() -> new BaseException(CompanyErrorCode.COMPANY_NOT_FOUND));
+
+        return CompanyResponseDto.from(company);
     }
 }
