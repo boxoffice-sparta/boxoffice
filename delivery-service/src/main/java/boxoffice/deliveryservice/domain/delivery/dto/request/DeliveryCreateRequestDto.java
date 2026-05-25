f4b0332c3b597a48de63c6d2fe204f23d059220a
GolemOnce <shh3350@gmail.com>
2026-05-25 22:47:37 +0900

    feat: delivery create용 DTO

---
+++ delivery-service/src/main/java/boxoffice/deliveryservice/domain/delivery/dto/request/DeliveryCreateRequestDto.java
@@ -0,0 +1,28 @@
+package boxoffice.deliveryservice.domain.delivery.dto.request;
+
+import com.boxoffice.common.entity.AddressVO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
+
+public record DeliveryCreateRequestDto(
+        @NotNull UUID orderId,
+        @NotNull UUID originHubId,
+        @NotNull UUID destinationHubId,
+        @Valid @NotNull AddressRequest deliveryAddress,
+        @NotBlank String recipientName,
+        String recipientSlackId
+) {
+    public record AddressRequest(
+            @Size(max = 10) String zipCode,
+            @NotBlank String address,
+            @Size(max = 255) String detailAddress
+    ) {
+        public AddressVO toAddressVO() {
+            return new AddressVO(zipCode, address, detailAddress);
+        }
+    }
+}
