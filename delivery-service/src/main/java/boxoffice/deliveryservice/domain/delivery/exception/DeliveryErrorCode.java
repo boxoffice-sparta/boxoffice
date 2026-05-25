915ca0e70710a3d0b31358669144a3bd0442a46d
GolemOnce <shh3350@gmail.com>
2026-05-25 22:48:32 +0900

    feat: Delivery, DeliveryRoute ErrorCode 추가

---
+++ delivery-service/src/main/java/boxoffice/deliveryservice/domain/delivery/exception/DeliveryErrorCode.java
@@ -0,0 +1,16 @@
+package boxoffice.deliveryservice.domain.delivery.exception;
+
+import com.boxoffice.common.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum DeliveryErrorCode implements ErrorCode {
+    DELIVERY_NOT_FOUND(HttpStatus.NOT_FOUND, "DELIVERY-001", "배송을 찾을 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
