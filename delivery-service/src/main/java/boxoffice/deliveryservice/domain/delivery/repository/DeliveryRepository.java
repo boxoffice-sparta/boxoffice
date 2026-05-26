0c539d6a6a659359e0446ca0bcc8e4e216d82736
GolemOnce <shh3350@gmail.com>
2026-05-26 12:57:55 +0900

    feat: 배송 경로 조회용 DTO

--- delivery-service/src/main/java/boxoffice/deliveryservice/domain/delivery/repository/DeliveryRepository.java
+++ delivery-service/src/main/java/boxoffice/deliveryservice/domain/delivery/repository/DeliveryRepository.java
@@ -2,12 +2,18 @@ package boxoffice.deliveryservice.domain.delivery.repository;
 
 
 import boxoffice.deliveryservice.domain.delivery.entity.Delivery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface DeliveryRepository extends JpaRepository<Delivery, UUID> {
+
+    Page<Delivery> findAllByDeletedAtIsNull(Pageable pageable);
+
+    Optional<Delivery> findByIdAndDeletedAtIsNull(UUID id);
 
     Optional<Delivery> findByOrderId(UUID orderId);
 }
