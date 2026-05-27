786929956d6fbf265aeda9c692d469fbd271b620
Ha Junyoung <jun19973@naver.com>
2026-05-27 12:04:35 +0900

    [fix] : 컨트롤러 @Valid 애노테이션 누락 추가, A.java 삭제,slackId 설계 개선 (Nullable 속성)

--- eureka-server/A.java
+++
@@ -1,6 +0,0 @@
-private final String username;
-    private final String email;
-    private final boolean enabled = true;
-    private final String firstName;
-    private final String lastName;
-    private final List<Credential> credentials;
