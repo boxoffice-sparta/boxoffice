64f5c96efe98c0e3472931495e14b6834986ff2b
주원 <juwon_0715@naver.com>
2026-05-30 23:13:52 +0900

    fix: 도커 컴포즈 포트 정상화 및 JPA 쿼리 파라미터 버그 수정

--- company-service/src/main/java/com/boxoffice/companyservice/product/repository/ProductRepository.java
+++ company-service/src/main/java/com/boxoffice/companyservice/product/repository/ProductRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
+import org.springframework.data.repository.query.Param;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
@@ -14,5 +15,5 @@ public interface ProductRepository extends JpaRepository<Product, UUID>, Product
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select p from Product p where p.id in :productIds order by p.id")
-    List<Product> findAllByIdInForUpdate(Collection<UUID> productIds);
+    List<Product> findAllByIdInForUpdate(@Param("productIds") Collection<UUID> productIds);
 }
